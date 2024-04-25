2941a7b9db74e8bfc88b7083333c2b6e88b08c95
Maria Kvashchuk <m_kvashchuk@icloud.com>
2024-04-26 00:49:58 +0200

    add search for functions

--- test_execution.py
+++ test_execution.py
@@ -10,13 +10,17 @@ def test_function(x): return (x[0] and x[1]) ^ (x[1] and x[2])
 
 
 def main():
-    qubits_number = 4
+    qubits_number = 5
 
     qc = QuantumCircuit(qubits_number)
     qc.h(0)
     qc.cx(0, 1)
-    qc.cx(0, 2)
-    qc.cx(0, 3)
+    qc.h(1)
+    qc.cx(1, 2)
+    qc.h(2)
+    qc.cx(2, 3)
+    qc.h(3)
+    qc.cx(3, 4)
 
     state = StabilizerState(qc)
 
@@ -26,29 +30,18 @@ def main():
 
     # TODO: check if f.__code__.co_stacksize is really a number of inputs
     function = BooleanFunction(num_args=test_function.__code__.co_stacksize)
-    truth_table = function.from_function_to_truth_table(test_function)
+    generic_truth_table = function.truth_table
     print("x1\tx2\tx3\tx1^x2\tx1^x3\tx2^x3\tx1^x2^x3\tf")  # TODO: see if there is any other way to do it nicely
-    print("\n".join(["\t".join([str(cell) for cell in row]) for row in truth_table]))
+    print("\n".join(["\t".join([str(cell) for cell in row]) for row in generic_truth_table]))
 
     start = time.time()  # TODO: get rid of time measurement
-    results = brute_force_connect(truth_table, stabilizers_eigen_values, qubits_number)
+    functions = brute_force_connect(function, stabilizers_eigen_values, qubits_number)
     end = time.time()
 
-    no_solutions = 0
-    for combination_result in results:
-        if len(combination_result) == 0:
-            no_solutions += 1
-    if no_solutions == len(results):
-        print("\nNo solutions")
-    else:
-        print("\nResults: ")
-        results_number = 1
-        for combination_results in results:
-            for result in combination_results:
-                print(results_number, ". Combination of columns: ", result["combination"], "\nRules for each column: ",
-                      [get_name_compare_choice(item) for item in result["choice"]],
-                      "\nComparison order for ev and function outcome: ", result["order"], "\n")
-                results_number += 1
+    results_number = 1
+    for function in functions:
+        print(results_number, ": ", function)
+        results_number += 1
 
     print("Elapsed: ", end - start)
 
