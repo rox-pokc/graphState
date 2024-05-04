5778b93d5cd60d00fcbba1487439e9ab7fce7ee1
Polina <ceasetoexistwithoutdying@gmail.com>
2024-05-04 23:52:17 +0200

    file stdout and functions for 4 inputs ghz3, ghz4

--- test_execution.py
+++ test_execution.py
@@ -5,26 +5,25 @@ from function.function_table import *
 from connect_state_function import *
 import time
 from datetime import datetime
+import sys
 
 
 def test_function(x): return (x[0] and x[1]) ^ (x[1] and x[2])
 
 
 def main():
-    qubits_number = 6
+    state_name = "GHZ"
+    qubits_number = 4
+    inputs_number = 4
 
     qc = QuantumCircuit(qubits_number)
     qc.h(0)
     qc.h(1)
     qc.h(2)
     qc.h(3)
-    qc.h(4)
-    qc.h(5)
     qc.cz(0, 1)
     qc.cz(0, 2)
     qc.cz(0, 3)
-    qc.cz(0, 4)
-    qc.cz(0, 5)
 
     state = StabilizerState(qc)
 
@@ -33,7 +32,7 @@ def main():
         print(stabilizer, "\t", stabilizers_eigen_values[stabilizer])
 
     # TODO: check if f.__code__.co_stacksize is really a number of inputs
-    function = BooleanFunction(num_args=test_function.__code__.co_stacksize)
+    function = BooleanFunction(num_args=inputs_number)
     generic_truth_table = function.truth_table
     print("x1\tx2\tx3\tx1^x2\tx1^x3\tx2^x3\tx1^x2^x3\tf")  # TODO: see if there is any other way to do it nicely
     print("\n".join(["\t".join([str(cell) for cell in row]) for row in generic_truth_table]))
@@ -46,13 +45,26 @@ def main():
     start = time.time()  # TODO: get rid of time measurement
     functions = brute_force_connect(function, stabilizers_eigen_values, qubits_number)
     end = time.time()
+  
+    # Save the current stdout so that we can revert sys.stdou after we complete
+    # our redirection
+    stdout_fileno = sys.stdout
+    # Redirect sys.stdout to the file
+    file_name = state_name + "_" + str(qubits_number) + "qubits_" + str(inputs_number) + "inputs.txt"
+    sys.stdout = open(file_name, 'w')
 
     results_number = 0
     for function in functions:
         print(function)
+        sys.stdout.write(function + '\n')
         results_number += 1
     print("Total: ", results_number)
     print("Elapsed: ", end - start)
+
+    # Close the file
+    sys.stdout.close()
+    # Restore sys.stdout to our old saved file handler
+    sys.stdout = stdout_fileno
 
 
 if __name__ == '__main__':
